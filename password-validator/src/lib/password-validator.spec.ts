1489b1ac04abbe402e4c1eaad579b2af913de768
alasdairsmith <alasdairsmith@>
2024-01-25 20:04:37 +0000

    Add attempt

--- password-validator/src/lib/password-validator.spec.ts
+++ password-validator/src/lib/password-validator.spec.ts
@@ -1,7 +1,33 @@
-import { passwordValidator } from './password-validator';
+import { passwordValidator, rules1} from './password-validator';
 
 describe('passwordValidator', () => {
   it('should work', () => {
-    expect(passwordValidator("Hello12!")).toEqual(true);
+    const result = passwordValidator("_Hello12!", rules1);
+    expect(result).toEqual(true);
   });
+
+  it('should have at least 8 chars', () => {
+    expect(passwordValidator("He_lo2!", rules1)).toEqual(false);
+  });
+
+  it('should contain a capital letter', () => {
+    expect(passwordValidator("hello12!", rules1)).toEqual(false);
+  });
+
+  it('should contain a lowercase letter', () => {
+    expect(passwordValidator("HELLO12!", rules1)).toEqual(false);
+  });
+  it('should contain a number', () => {
+    expect(passwordValidator("HEELLo!!", rules1)).toEqual(false);
+  })
+
+  it('should contain a _', () => {
+    expect(passwordValidator("HEELLo!!", rules1)).toEqual(false);
+  })
+
+  it('should satisfy rules2', () => {
+    expect(passwordValidator("HEo!!", rules1)).toEqual(false);
+  })
+
+
 });
--- password-validator/src/lib/password-validator.ts
+++ password-validator/src/lib/password-validator.ts
@@ -1,3 +1,22 @@
-export function passwordValidator(password: string): boolean {
-  throw Error('Unimplemented');
+import { Fn } from '@swc/core';
+
+const checkLengthBuilder = (length) => {
+  return (password) => password.length >= length;
+};
+
+const containsRegexBuilder = (regex) => {
+  return (password) => regex.test(password)
 }
+
+
+const isValidLength = checkLengthBuilder(8)
+const hasLowerCase = containsRegexBuilder(/[a-z]/)
+const hasUpperCase = containsRegexBuilder(/[A-Z]/)
+const hasUnderscore = containsRegexBuilder(/_/)
+const hasNumber = containsRegexBuilder(/\d/)
+export const rules1 = [isValidLength, hasLowerCase, hasUpperCase, hasUnderscore, hasNumber]
+export const rules2 = [checkLengthBuilder(6), hasLowerCase, hasUpperCase, hasNumber]
+export function passwordValidator(password: string, rules): boolean {
+  return rules.reduce((acc,fn) => fn(password) && acc, true)
+}
+
